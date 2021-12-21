383d7f725dfdea80062feb04530b0b7880aea3ef
parkerhuynh <dunghuynh110496@gmail.com>
2021-12-21 18:09:04 +1100

    fix data

--- text-to-CDQL/data_templates.py
+++ text-to-CDQL/data_templates.py
@@ -2,28 +2,28 @@
 templates = [
     {
         "text": "is the <A> in the <B> turning on?",
-        "query": 'pull (device.hasState.isOn) define entity device is from smarthome where device.deviceName = "<A>" and  device.isIn.buildingenvironment = "<B>"'
+        "query": 'pull (device.hasState.isOn) define entity device is from smarthome where device.deviceName = "<A>" and device.isIn.buildingenvironment = "<B>"'
     },
 
     {
         "text": "does the <A> in the <B> turn on?",
-        "query": 'pull (device.hasState.isOn) define entity device is from smarthome where device.deviceName = "<A>" and  device.isIn.buildingenvironment = "<B>"'
+        "query": 'pull (device.hasState.isOn) define entity device is from smarthome where device.deviceName = "<A>" and device.isIn.buildingenvironment = "<B>"'
     },
     {
         "text": "give me the state of the <A> in the <B>.",
-        "query": 'pull (device.hasState.isOn) define entity device is from smarthome where device.deviceName = "<A>" and  device.isIn.buildingenvironment = "<B>"'
+        "query": 'pull (device.hasState.isOn) define entity device is from smarthome where device.deviceName = "<A>" and device.isIn.buildingenvironment = "<B>"'
     },
     {
         "text": "in the <B>, give me the state of the <A>.",
-        "query": 'pull (device.hasState.isOn) define entity device is from smarthome where device.deviceName = "<A>" and  device.isIn.buildingenvironment = "<B>"'
+        "query": 'pull (device.hasState.isOn) define entity device is from smarthome where device.deviceName = "<A>" and device.isIn.buildingenvironment = "<B>"'
     },
     {
         "text": "in the <B>, is the <A> turning on.",
-        "query": 'pull (device.hasState.isOn) define entity device is from smarthome where device.deviceName = "<A>" and  device.isIn.buildingenvironment = "<B>"'
+        "query": 'pull (device.hasState.isOn) define entity device is from smarthome where device.deviceName = "<A>" and device.isIn.buildingenvironment = "<B>"'
     },
     {
         "text": "does the <A> in the <B> turn on.",
-        "query": 'pull (device.hasState.isOn) define entity device is from smarthome where device.deviceName = "<A>" and  device.isIn.buildingenvironment = "<B>"'
+        "query": 'pull (device.hasState.isOn) define entity device is from smarthome where device.deviceName = "<A>" and device.isIn.buildingenvironment = "<B>"'
     },
     {
         "text": "What is the current <A> level in the <B>.",
