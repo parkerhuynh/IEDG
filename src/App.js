00ff403d89c10432e6e0a84d06b07a3e530af8a7
NGOC DUNG HUYNH <ndhuynh@deakin.edu.au>
2022-01-17 13:08:13 +1100

    add wakeup-word, reset after 5s

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import { Container, Row, Form, Col } from "react-bootstrap"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
 
-
 function App() {
   const {
     transcript,
@@ -15,26 +14,54 @@ function App() {
   const [text, setText] = useState("")
   const [cdql, setcdql] = useState("")
   const [results, setResults] = useState("")
+  const wake_up_word = "Peter"
 
+  function checkword(word) {
+    return  word == wake_up_word;
+  }
+  const reset = () => {
+    resetTranscript()
+    setText()
+    setcdql()
+    setResults()
+  }
   useEffect(() => {
     SpeechRecognition.startListening({continuous: true})
   }, [])
 
   useEffect(() => {
     if (transcript.includes("reset")) {
-      resetTranscript()
+      reset()
     } else {
-      setText(transcript)
+      if (transcript.includes(wake_up_word)) {
+        const index = transcript.split(" ").findIndex(checkword)
+        var output_transcript = transcript.split(" ").slice(index+1)
+        output_transcript = output_transcript.join(" ")
+        setText(output_transcript)
+      }
+
     }
   }, [transcript, finalTranscript, resetTranscript])
-
+  
   useEffect(() => {
-    axios.post('/convert_text_to_cdql', {"text": finalTranscript}).then(res => {
-      setcdql(res["data"]["cdql"])
-    });
+    if (finalTranscript!= "" && finalTranscript.includes(wake_up_word)) {
+      const index = finalTranscript.split(" ").findIndex(checkword)
+      var output_finaltranscript = finalTranscript.split(" ").slice(index+1)
+      output_finaltranscript = output_finaltranscript.join(" ")
+      console.log(output_finaltranscript)
+
+      axios.post('/convert_text_to_cdql', {"text": output_finaltranscript}).then(res => {
+        setcdql(res["data"]["cdql"])
+        resetInt = setTimeout(()=> {
+          reset()
+        }, 5000)
+      })
+    };
+
   }, [finalTranscript])
 
   useEffect(() => {
+    
     axios.post('/convert_cdql_to_results', {"cdql": cdql}).then(res => {
       setResults(res["data"]["results"])
     });
